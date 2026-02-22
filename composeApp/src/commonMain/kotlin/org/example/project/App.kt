eb47f785cf2c98b0f208470ea1e3148cd7db59a4
iqbal <hossicoder@gmail.com>
2026-02-22 15:48:38 +0600

    updated

--- composeApp/src/commonMain/kotlin/org/example/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/App.kt
@@ -2,6 +2,7 @@ package org.example.project
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import org.example.project.presentation.screen.campaign.UpcomingCampaignScreen
 import org.example.project.presentation.screen.campaign.activeCampaign.ActiveCampaignScreen
 import org.example.project.presentation.screen.demo.DemoScreen
 import org.example.project.presentation.screen.login.LoginScreen
@@ -17,6 +18,7 @@ fun App() {
 //        SignUpScreen {}
 //        ActiveCampaignScreen()
 //        DemoScreen()
-        ActiveCampaignScreen()
+//        ActiveCampaignScreen()
+//        UpcomingCampaignScreen()
     }
 }
