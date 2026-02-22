dc3ffa06fca65f139a3f7717c44a057eb8de198e
RF-Rifat <rfrifat6344@gmail.com>
2026-02-22 15:46:33 +0600

    feat: Implement Upcoming Campaign and Weekly Checkin screens, update Android Gradle Plugin, and configure Foojay toolchain resolver.

--- composeApp/src/commonMain/kotlin/org/example/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/App.kt
@@ -3,6 +3,7 @@ package org.example.project
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import org.example.project.presentation.screen.campaign.activeCampaign.ActiveCampaignScreen
+import org.example.project.presentation.screen.campaign.weeklyCheckin.WeeklyCheckinScreen
 import org.example.project.presentation.screen.demo.DemoScreen
 import org.example.project.presentation.screen.login.LoginScreen
 import org.example.project.presentation.screen.signup.SignUpScreen
@@ -17,6 +18,7 @@ fun App() {
 //        SignUpScreen {}
 //        ActiveCampaignScreen()
 //        DemoScreen()
-        ActiveCampaignScreen()
+//        WeeklyCheckinScreen()
+        WeeklyCheckinScreen()
     }
 }
